45ff7d9afdd011e0e10fe37ad1d0f8a4ad9ce297
Jaspreet Singh <jaspreetsingh2379@gmail.com>
2018-06-04 12:22:22 +0530

    final Commit 3 june

--- routes/index.js
+++ routes/index.js
@@ -14,21 +14,55 @@ const opHelper = new OperationHelper({
   maxRequestsPerSecond: 1
 });
 
-var searchIndex =
-  /* GET home page. */
-  router.get('/', function (req, res, next) {
-    res.render('index', { data: [], title: 'Fill Form an Get Results'  })
-  });
+var searchIndexes = [
+  "All",
+  "Beauty",
+  "Grocery",
+  "Industrial",
+  "PetSupplies",
+  "OfficeProducts",
+  "Electronics",
+  "Watches",
+  "Jewelry",
+  "Luggage",
+  "Shoes",
+  "Furniture",
+  "KindleStore",
+  "Automotive",
+  "Pantry",
+  "MusicalInstruments",
+  "GiftCards",
+  "Toys",
+  "SportingGoods",
+  "PCHardware",
+  "Books",
+  "LuxuryBeauty",
+  "Baby",
+  "HomeGarden",
+  "VideoGames",
+  "Apparel",
+  "Marketplace",
+  "DVD",
+  "Appliances",
+  "Music",
+  "LawnAndGarden",
+  "HealthPersonalCare",
+  "Software"
+];
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { data: [], title: 'Fill Form an Get Results', searchIndexes: searchIndexes })
+});
 
 router.post('/', urlencodedParser, (req, res) => {
   opHelper.execute('ItemSearch', {
-    'SearchIndex': 'Books',
+    'SearchIndex': req.body.searchIndex,
     'Keywords': req.body.title,
     'ResponseGroup': 'ItemAttributes,Offers'
   }).then((response) => {
     const finalData = response.result.ItemSearchResponse.Items.Item
-    //res.send(finalData);
-    res.render('index', { title:'Results for Books named '+ req.body.title, data: finalData })
+    const finalTitle = 'Results in Category - ' + req.body.searchIndex + ' Search Term - ' + req.body.title
+    res.render('index', { title: finalTitle, data: finalData, searchIndexes: searchIndexes })
   }).catch((err) => {
     console.error("Something went wrong! ", err);
   });
